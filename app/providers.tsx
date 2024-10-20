0be84e99df7b231b4a40dd7677d551ae588a2a9f
VincenzoImp <vincenzoimperati99@gmail.com>
2024-10-20 17:17:22 +0200

    theme added

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,11 +1,18 @@
 "use client"
 
 import { NextUIProvider } from "@nextui-org/react"
+import { ThemeProvider  } from "next-themes"
 
 export default function Providers({ children }: { children: React.ReactNode }) {
-  return (
-    <NextUIProvider>
-      {children}
-    </NextUIProvider>
-  )
+	return (
+		<NextUIProvider>
+			<ThemeProvider
+				attribute="class"
+				defaultTheme="system"
+				themes={['dark', 'light']}
+			>
+				{children}
+			</ThemeProvider>
+		</NextUIProvider>
+	)
 }
