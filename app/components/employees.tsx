c912fa7ff5e9750833165d8ae56d74591e12c232
VincenzoImp <vincenzoimperati99@gmail.com>
2024-10-27 19:52:20 +0100

    fix typo, added firestore and storage object

--- app/components/employees.tsx
+++ app/components/employees.tsx
@@ -5,3 +5,60 @@ export default function Employees() {
         <h1 className="text-4xl font-bold text-center m-8">Employees</h1>
     );
 }
+
+// import { addDoc, collection, getDocs } from "firebase/firestore";
+// import { db } from "../firebase/config";
+
+// interface Employee {
+//     birthdate: Date;
+//     birthplace: string;
+//     birthplace_nation: string;
+//     birthplace_provincia: string;
+//     email: string;
+//     gender: string;
+//     livingplace_address: string;
+//     livingplace_nation: string;
+//     livingplace_provincia: string;
+//     livingplace_zipcode: number;
+//     n_mat: number;
+//     n_pro: number;
+//     name: string;
+//     phone: string;
+//     surname: string;
+//     tax_code: string;
+// }
+
+// async function pushEmployee(employee: Employee) {
+//     try {
+//         const docRef = await addDoc(collection(db, "employees"), employee);
+//         console.log("Document written with ID: ", docRef.id);
+//       } catch (e) {
+//         console.error("Error adding document: ", e);
+//       }
+// }
+
+// async function getEmployees() {
+//     const querySnapshot = await getDocs(collection(db, "employees"));
+//     return querySnapshot.docs.map((doc) => {
+//         const data = doc.data();
+//         return {
+//             ...data,
+//             birthdate: data.birthdate.toDate(),
+//             birthplace: data.birthplace,
+//             birthplace_nation: data.birthplace_nation,
+//             birthplace_provincia: data.birthplace_provincia,
+//             email: data.email,
+//             gender: data.gender,
+//             livingplace_address: data.livingplace_address,
+//             livingplace_nation: data.livingplace_nation,
+//             livingplace_provincia: data.livingplace_provincia,
+//             livingplace_zipcode: data.livingplace_zipcode,
+//             n_mat: data.n_mat,
+//             n_pro: data.n_pro,
+//             name: data.name,
+//             phone: data.phone,
+//             surname: data.surname,
+//             tax_code: data.tax_code
+//         };
+//     });
+// }
--- app/components/navigation.tsx
+++ app/components/navigation.tsx
@@ -6,78 +6,78 @@ import { useUser } from "../context";
 import { handleLogout } from "./authentication";
 
 export default function Navigation({ itemActive }: Readonly<{ itemActive: string }>) {
-  const [isMenuOpen, setIsMenuOpen] = useState(false);
-  const menuItems = [
-    "Home",
-    "Employees",
-    "Specializations"
-  ];
-  const { user, setUser } = useUser();
-  return (
-    <Navbar isBordered isMenuOpen={isMenuOpen} onMenuOpenChange={setIsMenuOpen}>
+	const [isMenuOpen, setIsMenuOpen] = useState(false);
+	const menuItems = [
+		"Home",
+		"Employees",
+		"Specializations"
+	];
+	const { user, setUser } = useUser();
+	return (
+		<Navbar isBordered isMenuOpen={isMenuOpen} onMenuOpenChange={setIsMenuOpen}>
 
-      <NavbarContent className="sm:hidden" justify="start">
-        <NavbarMenuToggle aria-label={isMenuOpen ? "Close menu" : "Open menu"} />
-      </NavbarContent>
+			<NavbarContent className="sm:hidden" justify="start">
+				<NavbarMenuToggle aria-label={isMenuOpen ? "Close menu" : "Open menu"} />
+			</NavbarContent>
 
-      <NavbarContent justify="center" className="sm:flex-1">
-        <NavbarBrand>
-          <p className="font-bold text-inherit">IWN</p>
-        </NavbarBrand>
-      </NavbarContent>  
+			<NavbarContent justify="center" className="sm:flex-1">
+				<NavbarBrand>
+					<p className="font-bold text-inherit">IWN</p>
+				</NavbarBrand>
+			</NavbarContent>  
 
-      <NavbarContent className="hidden sm:flex gap-4" justify="center">
-        {menuItems.map((item, index) => (
-          <NavbarItem key={`${item}-${index}`} isActive={itemActive === item}>
-            <Link
-              color={itemActive === item ? "primary" : "foreground"}
-              href={`/${item.toLowerCase()}`}
-              aria-current={itemActive === item ? "page" : undefined}
-            >
-              {item}
-            </Link>
-          </NavbarItem>
-        ))}
-      </NavbarContent>
+			<NavbarContent className="hidden sm:flex gap-4" justify="center">
+				{menuItems.map((item, index) => (
+					<NavbarItem key={`${item}-${index}`} isActive={itemActive === item}>
+						<Link
+							color={itemActive === item ? "primary" : "foreground"}
+							href={`/${item.toLowerCase()}`}
+							aria-current={itemActive === item ? "page" : undefined}
+						>
+							{item}
+						</Link>
+					</NavbarItem>
+				))}
+			</NavbarContent>
 
-      <NavbarContent as="div" justify="end">
-        <Dropdown placement="bottom-end">
-          <DropdownTrigger>
-            <Avatar
-              isBordered
-              as="button"
-              className="transition-transform"
-              size="sm"
-              showFallback
-            />
-          </DropdownTrigger>
-          <DropdownMenu aria-label="Profile Actions" variant="flat">
-            <DropdownItem key="profile" className="h-14 gap-2">
-              <p className="font-semibold">Signed in as</p>
-              <p className="font-semibold">{user?.email}</p>
-            </DropdownItem>
-            <DropdownItem key="logout" color="danger" onClick={() => handleLogout(setUser)}>
-              Log Out
-            </DropdownItem>
-          </DropdownMenu>
-        </Dropdown>
-      </NavbarContent>
+			<NavbarContent as="div" justify="end">
+				<Dropdown placement="bottom-end">
+					<DropdownTrigger>
+						<Avatar
+							isBordered
+							as="button"
+							className="transition-transform"
+							size="sm"
+							showFallback
+						/>
+					</DropdownTrigger>
+					<DropdownMenu aria-label="Profile Actions" variant="flat">
+						<DropdownItem key="profile" className="h-14 gap-2">
+							<p className="font-semibold">Signed in as</p>
+							<p className="font-semibold">{user?.email}</p>
+						</DropdownItem>
+						<DropdownItem key="logout" color="danger" onClick={() => handleLogout(setUser)}>
+							Log Out
+						</DropdownItem>
+					</DropdownMenu>
+				</Dropdown>
+			</NavbarContent>
 
-      <NavbarMenu>
-        {menuItems.map((item, index) => (
-          <NavbarMenuItem key={`${item}-${index}`}>
-            <Link
-              className="w-full"
-              color = { itemActive === item ? "primary" : "foreground" }
-              href={`/${item.toLowerCase()}`}
-              size="lg"
-            >
-              {item}
-            </Link>
-          </NavbarMenuItem>
-        ))}
-      </NavbarMenu>
-    </Navbar>
-  );
+			<NavbarMenu>
+				{menuItems.map((item, index) => (
+					<NavbarMenuItem key={`${item}-${index}`}>
+						<Link
+							className="w-full"
+							color = { itemActive === item ? "primary" : "foreground" }
+							href={`/${item.toLowerCase()}`}
+							size="lg"
+						>
+							{item}
+						</Link>
+					</NavbarMenuItem>
+				))}
+			</NavbarMenu>
+		</Navbar>
+	);
 }
 
