1ea119b52bc906f62ba20a023aa8c42699c4e588
VincenzoImp <vincenzoimperati99@gmail.com>
2024-10-27 15:02:25 +0100

    MILESTONE

--- app/components/specializations.tsx
+++ app/components/specializations.tsx
@@ -2,6 +2,6 @@
 
 export default function Specializations() {
     return (
-        <h1 className="text-4xl font-bold text-center m-8">Specialization</h1>
+        <h1 className="text-4xl font-bold text-center m-8">Specializations</h1>
     );
 }
