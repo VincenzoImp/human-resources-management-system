e865a8dc2aacfcce07db885a6ed992bd4f699bc2
VincenzoImp <vincenzoimperati99@gmail.com>
2024-12-01 16:55:35 +0100

    Update icons.tsx

--- app/icons.tsx
+++ app/icons.tsx
@@ -1,31 +1,5 @@
 import { JSX, SVGProps } from "react";
 
-const PlusIcon = (props : JSX.IntrinsicAttributes & SVGProps<SVGSVGElement>) => {
-    return (
-        <svg
-            aria-hidden="true"
-            fill="none"
-            focusable="false"
-            height="1em"
-            role="presentation"
-            viewBox="0 0 24 24"
-            width="1em"
-            {...props}
-        >
-            <g
-                fill="none"
-                stroke="currentColor"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth={1.5}
-            >
-                <path d="M6 12h12" />
-                <path d="M12 18V6" />
-            </g>
-        </svg>
-    );
-}
-
 const SearchIcon = (props: JSX.IntrinsicAttributes & SVGProps<SVGSVGElement>) => {
     return (
         <svg
@@ -54,27 +28,4 @@ const SearchIcon = (props: JSX.IntrinsicAttributes & SVGProps<SVGSVGElement>) =>
     );
 };
 
-const ChevronDownIcon = (props: JSX.IntrinsicAttributes & SVGProps<SVGSVGElement>) => {
-    return (
-        <svg
-            aria-hidden="true"
-            fill="none"
-            focusable="false"
-            height="1em"
-            role="presentation"
-            viewBox="0 0 24 24"
-            width="1em"
-            {...props}
-        >
-            <path
-                d="m19.92 8.95-6.52 6.52c-.77.77-2.03.77-2.8 0L4.08 8.95"
-                stroke="currentColor"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeMiterlimit={10}
-                strokeWidth={1.5} />
-        </svg>
-    );
-};
-
-export { PlusIcon, SearchIcon, ChevronDownIcon };
+export { SearchIcon };
