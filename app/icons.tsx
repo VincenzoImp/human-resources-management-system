1a7406d6ac412daece8bafaff18de9fde618ad25
VincenzoImp <vincenzoimperati99@gmail.com>
2025-01-20 09:55:52 +0100

    Update icons.tsx

--- app/icons.tsx
+++ app/icons.tsx
@@ -152,12 +152,24 @@ const NewWindowIcon = (props: JSX.IntrinsicAttributes & SVGProps<SVGSVGElement>)
             focusable="false"
             height="1em"
             role="presentation"
-            viewBox="0 0 18 18"
+            viewBox="0 0 22 22"
             width="1em"
             {...props}
         >
-            <path fill="#494c4e" d="M12.1.6a.944.944 0 0 0 .2 1.04l1.352 1.353L10.28 6.37a.956.956 0 0 0 1.35 1.35l3.382-3.38 1.352 1.352a.944.944 0 0 0 1.04.2.958.958 0 0 0 .596-.875V.96a.964.964 0 0 0-.96-.96h-4.057a.958.958 0 0 0-.883.6z"/>
-            <path fill="#494c4e" d="M14 11v5a2.006 2.006 0 0 1-2 2H2a2.006 2.006 0 0 1-2-2V6a2.006 2.006 0 0 1 2-2h5a1 1 0 0 1 0 2H2v10h10v-5a1 1 0 0 1 2 0z"/>
+            <path 
+                d="M8 21H20.4C20.7314 21 21 20.7314 21 20.4V3.6C21 3.26863 20.7314 3 20.4 3H3.6C3.26863 3 3 3.26863 3 3.6V16" 
+                stroke="currentColor"
+                strokeWidth={1.5}
+                strokeLinecap="round"
+                strokeLinejoin="round"
+            />
+            <path 
+                d="M3.5 20.5L12 12M12 12V16M12 12H8" 
+                stroke="currentColor"
+                strokeWidth={1.5}
+                strokeLinecap="round"
+                strokeLinejoin="round"
+            />
         </svg>
     );
 }
